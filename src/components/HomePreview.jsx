bb7b45e1ce45897a5a5fae3abec5a777e73c03af
OwlHD <m.a.flores02@gmail.com>
2023-09-14 16:36:34 -0700

    Product carousel change + home page populated & completed

--- src/components/HomePreview.jsx
+++ src/components/HomePreview.jsx
@@ -1,17 +1,38 @@
-import { Carousel } from '@trendyol-js/react-carousel';
-import ProductPreview from './ProductPreview';
-import 'react-multi-carousel/lib/styles.css';
+import ProductCarousel from './ProductCarousel';
+import getProductsByCategory from '../utils/functions';
+import { useState, useEffect } from 'react';
 
 export default function HomePreview() {
+    const [productsElectronics, setProductsElectronics] = useState([])
+    const [productsJewelry, setProductsJewelry] = useState([])
+    const [productsMensClothing, setProductsMensClothing] = useState([])
+    const [productsWomensClothing, setProductsWomensClothing] = useState([])
+    
+    useEffect(()=>{
+        async function getAllProductsByCategory(){
+            setProductsElectronics(await getProductsByCategory('electronics'))
+            setProductsJewelry(await getProductsByCategory('jewelery'))
+            setProductsMensClothing(await getProductsByCategory(`men's clothing`))
+            setProductsWomensClothing(await getProductsByCategory(`women's clothing`))
+        }
+
+        getAllProductsByCategory()
+    },[])
+
     return (
         <div>
-            <h1>test</h1>
-            <Carousel show={3.5} slide={3} swiping={true}>
-                <ProductPreview />
-                <ProductPreview />
-                <ProductPreview />
-                <ProductPreview />
-            </Carousel>
+            <h1>Electronics</h1>
+            <ProductCarousel products={productsElectronics} />
+            <br />
+            <h1>Jewelry</h1>
+            <ProductCarousel products={productsJewelry} />
+            <br />
+            <h1>Men's Clothing</h1>
+            <ProductCarousel products={productsMensClothing} />
+            <br />
+            <h1>Women's Clothing</h1>
+            <ProductCarousel products={productsWomensClothing} />
+
         </div>
     )
 }
--- src/components/ProductPreview.jsx
+++ src/components/ProductPreview.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-export default function ProductPreview() {
+export default function ProductPreview({item}) {
   return (
     <div>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://media.istockphoto.com/photos/aggressive-angry-dog-picture-id542218056?k=6&m=542218056&s=612x612&w=0&h=jSawPE-i2QHM-6i1YVCiDfuu4nFSTcAlvuIepnIUFk0=" />
+        <Card style={{ width: '18rem', height: '30rem' }}>
+            <Card.Img className='preview-image' variant="top" src={`${item.image}`} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>${item.price}</Card.Text>
+                <Button variant="primary">Add to Cart</Button>
             </Card.Body>
         </Card>
     </div>

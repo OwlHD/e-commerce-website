0063e2ec06cf7196d6d3328b2c10c64a87b567c4
OwlHD <m.a.flores02@gmail.com>
2023-09-28 19:33:11 -0700

    fixed price filter issue

--- src/components/Category/CategoryRender.jsx
+++ src/components/Category/CategoryRender.jsx
@@ -47,26 +47,26 @@ export default function CategoryRender({
                       ? id === 'all'
                         ? filters.rating === '0'
                           ? productsAll
-                            .filter((item) => item.price >= filters.price)
+                            .filter((item) => item.price <= filters.price)
                             .map((item) => <div key={item.id} className="col-xs-12 col-sm-6 col-lg-4"><CategoryCard key={item.id} item={item} cart={cart} /></div>)
                           : productsAll
-                            .filter((item) => item.rating.rate >= parseInt(filters.rating) && item.price >= filters.price)
+                            .filter((item) => item.rating.rate >= parseInt(filters.rating) && item.price <= filters.price)
                             .map((item) => <div key={item.id} className="col-xs-12 col-sm-6 col-lg-4"><CategoryCard key={item.id} item={item} cart={cart} /></div>)
                         : filters.rating === '0'
                           ? productsCategory
-                            .filter((item) => item.price >= filters.price)
+                            .filter((item) => item.price <= filters.price)
                             .map((item) => <div key={item.id} className="col-xs-12 col-sm-6 col-lg-4"><CategoryCard key={item.id} item={item} cart={cart} /></div>)
                           : productsCategory
-                            .filter((item) => item.rating.rate >= parseInt(filters.rating) && item.price >= filters.price)
+                            .filter((item) => item.rating.rate >= parseInt(filters.rating) && item.price <= filters.price)
                             .map((item) => <div key={item.id} className="col-xs-12 col-sm-6 col-lg-4"><CategoryCard key={item.id} item={item} cart={cart} /></div>)
                       : !queryProducts.list.length
                         ? 'No results found.'
                         : filters.rating === '0'
                           ? queryProducts.list
-                            .filter(item.price >= filters.price)
+                            .filter(item.price <= filters.price)
                             .map((item) => <div key={item.id} className="col-xs-12 col-sm-6 col-lg-4"><CategoryCard key={item.id} item={item} cart={cart} /></div>)
                           : queryProducts.list
-                            .filter((item) => item.rating.rate >= parseInt(filters.rating) && item.price >= filters.price)
+                            .filter((item) => item.rating.rate >= parseInt(filters.rating) && item.price <= filters.price)
                             .map((item) => <div key={item.id} className="col-xs-12 col-sm-6 col-lg-4"><CategoryCard key={item.id} item={item} cart={cart} /></div>)
                 )}
     </Row>
